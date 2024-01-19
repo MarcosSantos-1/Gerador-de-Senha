9bd6d3c79761e9315c2dc8428ad281f2e9471813
Marcos Vinicius <marcotas203@gmail.com>
2024-01-19 19:16:48 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let sliderElement = document.querySelector("#slide");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-passord");
+
+let charset= "";
